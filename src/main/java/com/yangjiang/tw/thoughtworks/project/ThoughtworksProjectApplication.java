e12f447ca58436b796d4db971a3822093a776b43
paul <122125597@qq.com>
2019-06-12 11:49:06 +0800

    first commit

---
+++ src/main/java/com/yangjiang/tw/thoughtworks/project/ThoughtworksProjectApplication.java
@@ -0,0 +1,13 @@
+package com.yangjiang.tw.thoughtworks.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThoughtworksProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThoughtworksProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yangjiang/tw/thoughtworks/project/ThoughtworksProjectApplicationTests.java
@@ -0,0 +1,16 @@
+package com.yangjiang.tw.thoughtworks.project;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ThoughtworksProjectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
